2e41f84e6c53c42a5a78c06c0fc473880f674a02
Abhishek <abhishekjha96500@gmail.com>
2025-08-17 19:28:45 +0530

    added hero section animation.

--- app/components/organism/Hero.tsx
+++ app/components/organism/Hero.tsx
@@ -6,6 +6,7 @@ import TwinkelThree from "~/assets/twinkel3";
 import { Button } from "../ui/button";
 import { NavLink } from "react-router";
 import Navbar from "../molecule/Navbar";
+import { motion } from "motion/react"
 
 export default function Hero() {
   return (
@@ -14,7 +15,18 @@ export default function Hero() {
         <Navbar />
       </div>
       <div className="flex w-full justify-between items-center">
-        <HeroSwirl />
+        {/* Floating Swirl */}
+        <motion.div
+          animate={{ y: [20, -20, 20], x: [0, -5, 0] }}
+          transition={{
+            duration: 8,
+            repeat: Infinity,
+            repeatType: "reverse",
+            ease: "easeInOut",
+          }}
+        >
+          <HeroSwirl />
+        </motion.div>
         <div className="flex flex-col gap-16 items-center">
           <HeroText />
           <p>We cover all kinds of categories and a weekly special guest.</p>
@@ -24,13 +36,61 @@ export default function Hero() {
             </Button>
           </NavLink>
         </div>
+
+        {/* Twinkels Floating Group */}
         <div className="w-2xs">
-          <TwinkelOne />
+          {/* Twinkel One */}
+          <motion.div
+            animate={{ 
+              y: [30, -20, 20], 
+              opacity: [0.8, 1, 0.85] // twinkle
+            }}
+            transition={{ 
+              duration: 5, 
+              repeat: Infinity, 
+              repeatType: "reverse", 
+              ease: "easeInOut" 
+            }}
+          >
+            <TwinkelOne />
+          </motion.div>
+
           <div className="flex">
-            <TwinkelThree />
-            <TwinkelTwo />
+            {/* Twinkel Three */}
+            <motion.div
+              animate={{ 
+                y: [0, -12, 0], 
+                x: [0, 8, 0], 
+                opacity: [0.7, 1, 0.75] // twinkle
+              }}
+              transition={{
+                duration: 4,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "easeInOut",
+              }}
+            >
+              <TwinkelThree />
+            </motion.div>
+
+            {/* Twinkel Two */}
+            <motion.div
+              animate={{ 
+                y: [10, -10, 0], 
+                opacity: [0.85, 1, 0.8] // twinkle
+              }}
+              transition={{
+                duration: 6.5, // slightly different for desync
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "easeInOut",
+              }}
+            >
+              <TwinkelTwo />
+            </motion.div>
           </div>
         </div>
+
       </div>
     </main>
   );
